fdabfd7d2e05eeea641dbc6b77938eca88febe2d
Jessica Tung <jessica.y.tung@gmail.com>
2014-05-31 16:43:33 -0700

    Add notes from peer pairing

---
+++ week_3/6_group_project/notes.js
@@ -0,0 +1,27 @@
+//Alternate sum
+arrayA.forEach(function(value) {sum += value;});
+
+//for loop sum
+		for (var i = 0; total = 0; i < array.A length; i++) {
+			total += arrayA[i];
+		}
+		return total;
+		
+		arrayA = [1,2,3];
+		"1+2+3"
+
+//alternate sum
+return array.length === 0 ? 0 : eval(arrayA.join('+'));
+
+//examples from median
+array = [1, 2, 3, 4]
+
+var middle = array.length / 2; // 2.5
+
+return array[middle - .5]; // array[2.5 - .5] => array[2]
+
+array.length = 4
+middle = 2
+
+return ( array[2] + array[1] ) / 2
+return (3 + 2) / 2; => 2.5
